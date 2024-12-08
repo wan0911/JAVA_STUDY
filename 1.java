4baca8ad557726bd57d3f6b09a35130a050591c4
wan <106522971+wan0911@users.noreply.github.com>
2024-12-09 08:55:54 +0900

    initial commit

---
+++ 1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String words = in.nextLine();
+        char word = in.next().charAt(0);
+        int cnt = 0;
+
+        words = words.toUpperCase();
+        word = Character.toUpperCase(word);
+
+        //String[] arr = words.split("");
+        //System.out.println(Arrays.toString(arr));
+
+
+        for (int i=0; i < words.length(); i++) {
+            if (words.charAt(i) == word) {
+                 cnt++;
+            }
+        }      
+
+        // for (int i=0; i < arr.length; i++) {
+        //     if (arr[i] == word) cnt++;
+        // }
+        System.out.println(cnt);
+    }
+}
